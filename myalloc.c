85de7993872c2c2f2c884d21bb75d55f1c2e4abd
Melissa Swearingen <swearingenmj@gmail.com>
2022-02-27 22:09:21 -0800

    changed if statement in myfree() to 'if cur is not in_use and next node not in use'

--- myalloc.c
+++ myalloc.c
@@ -64,11 +64,10 @@ void myfree(void *p) {
         // Merge free blocks
         current = head;
         while ((current->next) != NULL) {
-            if ((current->in_use) && (current->next->in_use)) {
+            if ((current->in_use = 0) && (current->next->in_use == 0)) {
                 current->size += (current->next->size) + sizeof(struct block);
                 current->next = current->next->next;
             } else { 
-                previous = current;
                 current = current->next;
             }
         }
@@ -134,9 +133,10 @@ void *myalloc(size_t size) {
 int main(void){ 
     void *p;
 
-    print_data();
-    p = myalloc(16);
-    print_data();
-    p = myalloc(16);
-    printf("%p\n", p);
+    myalloc(10);     print_data();
+    p = myalloc(20); print_data();
+    myalloc(30);     print_data();
+    myfree(p);       print_data();
+    myalloc(40);     print_data();
+    myalloc(10);     print_data();
 }
