1815c2dd1b830557a3dc8e5f5ba4180eca7c9114
Melissa Swearingen <swearingenmj@gmail.com>
2022-03-12 23:54:18 -0800

    made changes to myfree function and myalloc

--- myalloc.c
+++ myalloc.c
@@ -8,7 +8,6 @@
 #define PADDED_SIZE(x) ((x) + GET_PAD(x))
 #define PTR_OFFSET(p, offset) ((void*)((char *)(p) + (offset)))
 
-// make linked list inside the above data space
 struct block *head = NULL;  // Head of the list, empty
 
 struct block {
@@ -42,7 +41,6 @@ void print_data(void) {
 void split(struct block *b, size_t size){
 
     struct block *new = PTR_OFFSET(b, size + PADDED_SIZE(sizeof(struct block)));
-    //void*)((void*)fitting_slot+size+sizeof(struct block));
     new->size = (b->size) - size - sizeof(struct block);
     new->in_use = 1;
     new->next = b->next;
@@ -53,37 +51,30 @@ void split(struct block *b, size_t size){
 }
 
 void myfree(void *p) {
-    char memory[20000];
-    struct block *current, *previous;
 
-    if (((void*) memory <= p) && (p <= (void*)(memory + 20000))) {
-        struct block *current = p;
-        --current;
-        current->in_use=1;
-        
-        // Merge free blocks
-        current = head;
-        while ((current->next) != NULL) {
-            if ((current->in_use = 0) && (current->next->in_use == 0)) {
-                current->size += (current->next->size) + sizeof(struct block);
-                current->next = current->next->next;
-            } else { 
-                current = current->next;
-            }
-        }
+    // start cur at head
+    struct block *current = head;
 
-    } else {
-        printf("Provide value pointer allocated by myalloc\n");
+    // while cur->next isn't NULL:
+    while (current->next != NULL) {
+
+        // if cur is not in_use and next node not in use:
+        if ((current->in_use == 0) && (current->next->in_use == 0)) {
+
+            // add the next node's region's size to cur's
+            current->size += (current->next->size) + sizeof(struct block);
+
+            // make cur's next pointer skip the next node
+            current->next = current->next->next;
+
+        } else {
+            // mov cur to next node
+            current = current->next;
+        }   
     }
 }
 
-void *myalloc(size_t size) {
-
-    // use sbrk to allocate a chunk of data space
-    // node information:
-        // in-use(true or false)?
-        // size of allocated memory region
-        // pointer to the next memory region
+void *myalloc(int size) {
 
     if (head == NULL) {
         head = sbrk(1024);
@@ -92,42 +83,27 @@ void *myalloc(size_t size) {
         head->in_use = 0;
     }
 
-    // walk the list looking for a free node
-    //     let n = the free node
-    //     if n's data is big enough for the user request
-    //         mark n as used
-    //         return address of n's data
+    struct block *current = head;
+    int padded_size = PADDED_SIZE(size);
 
-    // I used this website to guide the construction of the myalloc method body:
-        //http://tharikasblogs.blogspot.com/p/how-to-write-your-own-malloc-and-free.html
+    // Walk the list looking for a free node
+    while (current != NULL) {
 
-    struct block *current, *previous;
+        // If n's data big enough for user request
+        if (current->in_use == 0 && current->size >= padded_size) {
+            
+            // Mark n as used
+            current->in_use = 1;
 
-    while (((current->size) < size) || (((current->in_use) == 1) && (current->next != NULL))) {
-        current = head;
-        previous = current;
-        current = current->next;
+            // Return address of n's data plus padding
+            int padded_block_size = PADDED_SIZE(sizeof(struct block));
+            return PTR_OFFSET(current, padded_block_size);
+        }
     }
 
-    if ((current->size) == size) {
-        current->in_use = 1;
-    } else if ((current->size) > (size + sizeof(struct block))) {
-        split(current, size);
-        current->in_use = 1;
-    } else {
-        // if there was no room
-        // return NULL
-        return NULL;
-    }
-
-    // what we are returning:
-    // malloc will return a pointer to the data.. NOT a pointer to the struct block
-    // the data start at n bytes past the struct, where n is PADDED_SIZE(sizeof(struct block))
-    // padded_struct_block_size = PADDED_SIZE(sizeof(struct block));
-
-    // ... All the machinations to allocate go here ...
-    int padded_block_size = PADDED_SIZE(sizeof(struct block));
-    return PTR_OFFSET(current, padded_block_size);
+    // If we get here, there was no room
+    // Return NULL
+    return NULL;
 }
 
 int main(void){ 
